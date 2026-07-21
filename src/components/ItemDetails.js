2573d6fc4e8110d3d85752927958ca901f78d97e
anideva <anidevarchowdhury@gmial.com>
2026-07-21 12:56:46 +0530

    used dynamic routing to retrive the idem details or data

---
+++ src/components/ItemDetails.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {useParams} from "react-router-dom";
+
+function ItemDetails(){
+
+    const items =[
+        {
+            id:"1",
+            name:"Item 1",
+            description:"Description for Item 1"
+        },
+         {
+            id:"2",
+            name:"Item 2",
+            description:"Description for Item 2"
+        },
+         {
+            id:"3",
+            name:"Item 3",
+            description:"Description for Item 3"
+        },
+        
+    ];
+    const {id}= useParams();
+    const item = items.find((item)=> item.id===id);
+    return (
+        <div>
+            <h1>{item.name}</h1>
+            <p>{item.description}</p>
+        </div>
+
+    );
+}
+
+export default ItemDetails;
